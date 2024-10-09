622c6725bce1654bc4c10a4c6e8d89d8e07a7159
Uros Mrkobrada <uros.mrkobrada@gmail.com>
2024-10-09 15:52:03 +0200

    update index.js

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const installation = process.spawn(".venv/bin/python", ['-m', 'pip', 'install', 
 installation.stderr.on('close', (code) => {
   console.log("upgrade aijson-meta")
 })
+installation.stdout.on("data", (data) => {
+  // console.log(data.toString())
+})
